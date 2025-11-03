9dc1cd7aa35781809a6385910113b71adb4e5c60
raflyhandika <112202407183@mhs.dinus.ac.id>
2025-11-03 13:07:41 +0700

    first commit

--- script.js
+++ script.js
@@ -81,3 +81,12 @@ document.querySelectorAll(".buy-btn").forEach((btn) => {
   });
 });
 
+function showTime() {
+  const now = new Date();
+  const time = now.toLocaleTimeString();
+  document.getElementById("clock").textContent = " | " + time;
+}
+setInterval(showTime, 1000);
+showTime();
+
+
